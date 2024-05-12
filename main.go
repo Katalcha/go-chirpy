ba3bf9ac5242af08c78436e477eb23274081b450
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-05-12 23:28:25 +0200

    add FileServer
    submit 1.5

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"net/http"
 )
 
+const (
+	ROOT string = "."
+	PORT string = "8080"
+)
+
 func main() {
 	serveMux := http.NewServeMux()
-	httpServer := http.Server{
-		Addr:    "localhost:8080",
+	serveMux.Handle("/", http.FileServer(http.Dir(ROOT)))
+
+	httpServer := &http.Server{
+		Addr:    "localhost:" + PORT,
 		Handler: serveMux,
 	}
+
 	httpServer.ListenAndServe()
 }
