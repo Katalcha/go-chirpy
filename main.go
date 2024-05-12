1efcf3fecdb887164038b85609244c44dda1dab5
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-05-12 23:47:32 +0200

    submit 1.8

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 const (
 	ROOT string = "."
 	PORT string = "8080"
+	LOGO string = "/assets/logo.png"
 )
 
 func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", http.FileServer(http.Dir(ROOT)))
+	serveMux.Handle("/assets", http.FileServer(http.Dir(LOGO)))
 
 	httpServer := &http.Server{
 		Addr:    "localhost:" + PORT,
