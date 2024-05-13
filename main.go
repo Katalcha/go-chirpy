535dbe5749b38cf301489d4f2b053f046a66a5cd
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-05-13 18:15:51 +0200

    submit 1.11
    add readinessHandler

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 const (
-	ROOT string = "."
-	PORT string = "8080"
-	LOGO string = "/assets/logo.png"
+	ROOT_PATH string = "."
+	PORT      string = "8080"
+	HEALTHZ   string = "/healthz"
 )
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(ROOT)))
-	serveMux.Handle("/assets", http.FileServer(http.Dir(LOGO)))
+	serveMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(ROOT_PATH))))
+	serveMux.HandleFunc(HEALTHZ, healthzHandler)
 
 	httpServer := &http.Server{
 		Addr:    "localhost:" + PORT,
 		Handler: serveMux,
 	}
 
-	httpServer.ListenAndServe()
+	log.Printf("Serving Yo Mama from %s on port: %s\n", ROOT_PATH, PORT)
+	log.Fatal(httpServer.ListenAndServe())
+}
+
+func healthzHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
 }
